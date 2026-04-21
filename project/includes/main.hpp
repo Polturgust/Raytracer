f13a042eb08cb3ff3fe454ad93b64b524e0e28f1
eric Faure-duand-bruschi <eric.faure-dunand-bruschi@epitech.eu>
2026-04-21 16:05:22 +0200

    [+] init repo

---
+++ project/includes/main.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** raytracer
+** File description:
+** main.hpp
+*/
+
+#ifndef MAIN_HPP
+    #define MAIN_HPP
+    
+
+#endif
---
+++ project/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** raytracer
+** File description:
+** main.cpp
+*/
+
+#include "main.hpp"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    return 0;
+}

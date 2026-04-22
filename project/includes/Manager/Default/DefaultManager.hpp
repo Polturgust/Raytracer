0e5694ab885e5b959fa03e86878f21c4be5e3a2b
eric Faure-duand-bruschi <eric.faure-dunand-bruschi@epitech.eu>
2026-04-22 23:17:59 +0200

    [+] added default Manager

---
+++ project/includes/Manager/Default/DefaultManager.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** raytracer
+** File description:
+** DefaultManager.hpp
+*/
+
+#ifndef DEFAULTMANAGER_HPP
+    #define DEFAULTMANAGER_HPP
+
+    #include "AManager.hpp"
+
+namespace raytracer {
+
+class DefaultManager : public AManager {
+public:
+    DefaultManager() = default;
+    ~DefaultManager() = default;
+};
+
+}
+
+#endif

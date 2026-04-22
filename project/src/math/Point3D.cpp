05329f217da9cca01153ed30d1961a23ea6408f4
Flo <florent.serra@epitech.eu>
2026-04-22 20:34:52 +0200

    [+] Add Point3D math implementation

---
+++ project/src/math/Point3D.cpp
@@ -0,0 +1,41 @@
+#include "rt/math/Point3D.hpp"
+
+namespace raytracer::math {
+
+Point3D::Point3D(double xValue, double yValue, double zValue)
+    : x(xValue), y(yValue), z(zValue)
+{
+}
+
+Point3D Point3D::operator+(const Vector3D &vector) const
+{
+    return Point3D(x + vector.x, y + vector.y, z + vector.z);
+}
+
+Point3D &Point3D::operator+=(const Vector3D &vector)
+{
+    x += vector.x;
+    y += vector.y;
+    z += vector.z;
+    return *this;
+}
+
+Point3D Point3D::operator-(const Vector3D &vector) const
+{
+    return Point3D(x - vector.x, y - vector.y, z - vector.z);
+}
+
+Point3D &Point3D::operator-=(const Vector3D &vector)
+{
+    x -= vector.x;
+    y -= vector.y;
+    z -= vector.z;
+    return *this;
+}
+
+Vector3D Point3D::operator-(const Point3D &other) const
+{
+    return Vector3D(x - other.x, y - other.y, z - other.z);
+}
+
+} // namespace raytracer::math

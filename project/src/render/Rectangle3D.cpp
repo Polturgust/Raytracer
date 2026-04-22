8fa5491fcd2a61d34a41932253a74b04e896eef6
Flo <florent.serra@epitech.eu>
2026-04-22 20:34:53 +0200

    [+] Add Rectangle3D render implementation

---
+++ project/src/render/Rectangle3D.cpp
@@ -0,0 +1,18 @@
+#include "rt/render/Rectangle3D.hpp"
+
+namespace raytracer::render {
+
+Rectangle3D::Rectangle3D(
+    const math::Point3D &originPoint,
+    const math::Vector3D &width,
+    const math::Vector3D &height)
+    : origin(originPoint), widthVector(width), heightVector(height)
+{
+}
+
+math::Point3D Rectangle3D::pointAt(double u, double v) const
+{
+    return origin + (widthVector * u) + (heightVector * v);
+}
+
+} // namespace raytracer::render

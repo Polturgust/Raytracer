16f103d3f932fa23ca285b4ca9e1241c4fb4f787
Eric Faure Dunand Bruschi <eric.faure-dunand-bruschi@epitech.eu>
2026-05-09 02:25:46 +0200

    [+] added more function in printer

--- project/includes/Display/printer/printer.hpp
+++ project/includes/Display/printer/printer.hpp
@@ -12,12 +12,19 @@
 namespace raytracer {
 
 class Printer {
+    std::size_t LineWrite = 0;
 public:
     Printer() = default;
     ~Printer() = default;
 
-    void moveCursorUp(int n) {std::cout << "\033[" << n << "A";};
-    void clearLine() {std::cout << "\033[2K\r";}
+    void MoveCursorUp(int n) {std::cout << "\033[" << n << "A";};
+    void ClearLine() {std::cout << "\033[2K\r";};
+    void PrintLine(std::string line) {std::cout << line << std::endl; LineWrite++;};
+    void PrintLine(std::string line, int nb_line) {std::cout << line << std::endl; LineWrite += nb_line;};
+
+    void ClearNbLine(int nb) {for (int i = nb; i > 0; i--) {std::cout << "\033[1A"; std::cout << "\033[2K\r";}};
+    void ClearToStart() {this->ClearNbLine(LineWrite); LineWrite = 0;};
+    void SetStart() {LineWrite = 0;};
 };
 
 }

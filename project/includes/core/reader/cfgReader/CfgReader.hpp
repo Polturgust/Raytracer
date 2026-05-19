f1cc5617365db6354d854b523ab382225ab48933
Rofe <raphael.guillot@epitech.eu>
2026-05-19 11:01:43 +0200

    {11:00} | [#] Corrected the parsing, so no more SIGABORT on exec

--- project/includes/core/reader/cfgReader/CfgReader.hpp
+++ project/includes/core/reader/cfgReader/CfgReader.hpp
@@ -32,30 +32,21 @@ class CfgReader : public AReader {
         const libconfig::Setting& objects = _cfg.lookup(name_list);
         for (int i = 0; i < objects.getLength(); ++i) {
             const libconfig::Setting& group = objects[i];
-            std::string GroupeName = group.getName();
-            std::map<std::string, std::string> param;
-            if (group.getLength() > 0 && group[0].isGroup()) {
-                for (int j = 0; j < group.getLength(); ++j) {
-                    param = settingToParams(group[j]);
-                    try {
-                        list.push_back(ldloader.load<T>(GroupeName, param));
-                    } catch (const IError& e) {
-                        if (e.code() == 0 && std::find(NoOpenFile.begin(), NoOpenFile.end(), GroupeName) == NoOpenFile.end())
-                            NoOpenFile.push_back(GroupeName);
-                        if (e.code() == 84)
-                            std::cout << Color::RED << "Error " << Color::RESET << e.what() << std::endl;
-                    }
-                }
-            } else {
-                param = settingToParams(group);
-                try {
-                    list.push_back(ldloader.load<T>(GroupeName, param));
-                } catch (const IError& e) {
-                    if (e.code() == 0 && std::find(NoOpenFile.begin(), NoOpenFile.end(), GroupeName) == NoOpenFile.end())
-                        NoOpenFile.push_back(GroupeName);
-                    if (e.code() == 84)
-                        std::cout << Color::RED << "Error " << Color::RESET << e.what() << std::endl;
-                }
+            const char *rawName = group.getName();
+            std::string groupName = rawName ? rawName : "";
+            if (groupName.empty()) {
+                if (!group.lookupValue("type", groupName) || groupName.empty())
+                    throw Warning("Reader: missing plugin type in scene file.\n");
+            }
+
+            std::map<std::string, std::string> param = settingToParams(group);
+            try {
+                list.push_back(ldloader.load<T>(groupName, param));
+            } catch (const IError& e) {
+                if (e.code() == 0 && std::find(NoOpenFile.begin(), NoOpenFile.end(), groupName) == NoOpenFile.end())
+                    NoOpenFile.push_back(groupName);
+                if (e.code() == 84)
+                    std::cout << Color::RED << "Error " << Color::RESET << e.what() << std::endl;
             }
         }
         return list;
